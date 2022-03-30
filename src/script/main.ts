d8060b9086914aecb02130bfe538f2286fa43f55
felixrydberg <felixryd@gmail.com>
2022-03-30 12:02:50 +0200

    [Add] Basic structure for nav

--- src/script/main.ts
+++ src/script/main.ts
@@ -1,4 +1,27 @@
-import { db } from "./modules/db";
+import { db } from './modules/db';
 
-console.log(db)
+(function () {
+  (function () {
+    const parent: HTMLElement = document.querySelector('.nav');
 
+    const home: HTMLElement = document.createElement('a');
+    const homeImg: HTMLElement = document.createElement('img');
+
+    const contact: HTMLElement = document.createElement('a');
+    const contactImg: HTMLElement = document.createElement('img');
+
+    if (sessionStorage.getItem('login')) {
+      const profile: HTMLElement = document.createElement('a');
+      const profileImg: HTMLElement = document.createElement('img');
+
+      const logout: HTMLElement = document.createElement('a');
+      const logoutImg: HTMLElement = document.createElement('img');
+    } else {
+      const signin: HTMLElement = document.createElement('a');
+      const signinImg: HTMLElement = document.createElement('img');
+
+      const signup: HTMLElement = document.createElement('a');
+      const signupImg: HTMLElement = document.createElement('img');
+    }
+  })();
+})();
