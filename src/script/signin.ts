cd39ad4de1c4b12cafdd2536f703a24148971f4d
felixrydberg <felixryd@gmail.com>
2022-03-31 13:34:05 +0200

    [Add] Logoutbtn

--- src/script/signin.ts
+++ src/script/signin.ts
@@ -1,18 +1,15 @@
-import { checkPwd, userAvailable } from "./modules/signfunctions";
-import navToggle from "./modules/navtoggle";
+import { checkPwd, userAvailable } from './modules/signfunctions';
 
 (function () {
-  navToggle();
-
   // Sätter Addeventlistner på Formen
   (function () {
-    const form: HTMLElement = document.querySelector(".form-signin");
+    const form: HTMLElement = document.querySelector('.form-signin');
 
-    form.addEventListener("submit", (event) => {
+    form.addEventListener('submit', (event) => {
       event.preventDefault();
 
-      const name: HTMLInputElement = document.querySelector(".form-name");
-      const pwd: HTMLInputElement = document.querySelector(".form-pwd");
+      const name: HTMLInputElement = document.querySelector('.form-name');
+      const pwd: HTMLInputElement = document.querySelector('.form-pwd');
 
       checkUser(name.value.toLowerCase(), pwd.value);
     });
@@ -21,9 +18,9 @@ import navToggle from "./modules/navtoggle";
   // Kollar om användaren uppfyller x antal krav och om lösenord matchar
   const checkUser = async (name, pwd) => {
     if (await !userAvailable(name)) {
-      console.log("User doesnt exsist");
+      console.log('User doesnt exsist');
     } else if (await checkPwd(name, pwd)) {
-      console.log("pwd doesnt match username");
+      console.log('pwd doesnt match username');
     } else {
       loginUser(name);
     }
@@ -32,9 +29,9 @@ import navToggle from "./modules/navtoggle";
   // Skapar session variablerna och skickar användaren till index.html
   const loginUser = (name) => {
     sessionStorage.clear();
-    sessionStorage.setItem("login", "true");
-    sessionStorage.setItem("name", name);
+    sessionStorage.setItem('login', 'true');
+    sessionStorage.setItem('name', name);
 
-    window.location.replace("../");
+    window.location.replace('../');
   };
 })();
