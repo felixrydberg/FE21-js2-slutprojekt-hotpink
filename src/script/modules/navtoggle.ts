cef927eb0677c1bc34b56e7fde1bc0f192fc1440
felixrydberg <felixryd@gmail.com>
2022-04-01 12:19:16 +0200

    [Add] Replaced Profilebtn with Signupbtn
    If user is not logged in:
    -Home
    -Sign in
    -Sign up
    
    If user is logged in:
    -Home
    -Profile
    -Logout

--- src/script/modules/navtoggle.ts
+++ src/script/modules/navtoggle.ts
@@ -7,8 +7,17 @@ export default function navToggle(): void {
         sessionStorage.setItem('profile', sessionStorage.getItem('name'));
         window.location.replace('../profile.html');
       });
+
     let btnsignIn: HTMLAnchorElement = document.querySelector('#signinbtn');
     btnsignIn.style.display = 'none';
+
+    // Slutar visa signup om man är inloggad
+    let btnsignUp: HTMLAnchorElement = document.querySelector('#signupbtn');
+    btnsignUp.style.display = 'none';
+
+    // Om man är inloggad visas denna knapp.
+    let profilebtn: HTMLAnchorElement = document.querySelector('#profile');
+    profilebtn.style.display = 'block';
 
     // Om man är inloggad visas denna knapp. Om man trycker på den tas variabler i SessionStorage bort och sidan laddas om
     let btnlogout: HTMLAnchorElement = document.querySelector('#logoutbtn');
