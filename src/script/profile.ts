11d8376ed0cd5092a162aa74af4773e0028b4838
Jimmy Wallener <38855083+JimmyWallener@users.noreply.github.com>
2022-04-01 15:45:33 +0200

    [Refactor] commenting and refactoring

--- src/script/profile.ts
+++ src/script/profile.ts
@@ -3,41 +3,49 @@ import { db } from './modules/db';
 import navToggle from './modules/navtoggle';
 
 ((): void => {
-  if (!sessionStorage.getItem('login')) {
+  // If user is logged in
+  if (sessionStorage.getItem('login')) {
+    const getUser = async (): Promise<void> => {
+      // catch correct username to display
+      const userName =
+        sessionStorage.getItem('name') === null
+          ? sessionStorage.getItem('name')
+          : sessionStorage.getItem('profile');
+
+      await get(ref(db, `/users/${userName.toLowerCase()}`)).then(
+        (snapshot: DataSnapshot): void => {
+          if (snapshot.exists()) {
+            const { bio, img, username } = snapshot.val();
+            let firstName = document.getElementById('username');
+            let biography = document.getElementById('bio');
+            let profilePicture = <HTMLImageElement>(
+              document.getElementById('profilepic')
+            );
+            profilePicture.src = img;
+            firstName.innerText = username;
+            biography.innerText = bio;
+
+            // If user is owner of profile, add listener that allows user to delete account
+            // clear sessionstorage and sends user to startpage
+            if (sessionStorage.getItem('name') === userName) {
+              const removeAccount: HTMLButtonElement =
+                document.querySelector('.remove-profile');
+              removeAccount.addEventListener('click', (e: MouseEvent): void => {
+                e.preventDefault();
+                const dbRef = ref(db, `/users/${userName}`);
+                remove(dbRef);
+                sessionStorage.clear();
+                window.location.replace('../index.html');
+              });
+            }
+          }
+        }
+      );
+    };
+    getUser();
+  } else {
     window.location.replace('/');
   }
-  const getUser = (): void => {
-    const userName =
-      sessionStorage.getItem('name') === null
-        ? sessionStorage.getItem('name')
-        : sessionStorage.getItem('profile');
-    get(ref(db, `/users/${userName}`)).then((snapshot: DataSnapshot): void => {
-      if (snapshot.exists()) {
-        const { bio, img, username } = snapshot.val();
-        let firstName = document.getElementById('username');
-        let biography = document.getElementById('bio');
-        let profilePicture = <HTMLImageElement>(
-          document.getElementById('profilepic')
-        );
 
-        profilePicture.src = img;
-        firstName.innerText = username;
-        biography.innerText = bio;
-
-        if (sessionStorage.getItem('name') === userName) {
-          const removeAccount: HTMLButtonElement =
-            document.querySelector('.remove-profile');
-          removeAccount.addEventListener('click', (e: MouseEvent): void => {
-            e.preventDefault();
-            const dbRef = ref(db, `/users/${userName}`);
-            remove(dbRef);
-            sessionStorage.clear();
-            window.location.replace('../index.html');
-          });
-        }
-      }
-    });
-  };
-  getUser();
   navToggle();
 })();
